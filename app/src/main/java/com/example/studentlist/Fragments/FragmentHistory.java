982e208b39692b7b1287575faee10a3c76e99cca
Jetro <jetropapa@gmail.com>
2025-03-06 00:37:47 +0800

    complete admin menus

--- app/src/main/java/com/example/studentlist/Fragments/FragmentHistory.java
+++ app/src/main/java/com/example/studentlist/Fragments/FragmentHistory.java
@@ -12,10 +12,10 @@ import com.example.studentlist.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentEditProfile#newInstance} factory method to
+ * Use the {@link FragmentHistory#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentEditProfile extends Fragment {
+public class FragmentHistory extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class FragmentEditProfile extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragmentEditProfile() {
+    public FragmentHistory() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class FragmentEditProfile extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentEditProfile.
+     * @return A new instance of fragment FragmentHistory.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentEditProfile newInstance(String param1, String param2) {
-        FragmentEditProfile fragment = new FragmentEditProfile();
+    public static FragmentHistory newInstance(String param1, String param2) {
+        FragmentHistory fragment = new FragmentHistory();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class FragmentEditProfile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        return inflater.inflate(R.layout.fragment_history, container, false);
     }
 }
